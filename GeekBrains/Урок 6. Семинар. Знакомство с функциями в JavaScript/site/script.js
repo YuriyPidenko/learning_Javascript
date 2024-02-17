32cdc1b68201778b39ea4dac87edb9a7e632581e
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-02-17 17:34:12 +0300

    dz03

---
+++ GeekBrains/Урок 6. Семинар. Знакомство с функциями в JavaScript/site/script.js
@@ -0,0 +1,60 @@
+// function numberСube(num) {
+//     return num * num * num;
+// }
+
+// let num = Number(prompt('Введите число'));
+// console.log(numberСube(num));
+
+
+
+// let mony = Number(prompt('Ваша зарабатная плата ?'));
+
+
+// if (mony > 0) {
+//     const yourWage = (mony) => {
+//             mony = mony * 0.87
+//             return mony;
+//         }
+//         // function yourWage(mony) {
+//         //     mony = mony * 0.87
+//         //     return mony;
+//         // }
+//     console.log(`Ваша зарплата с вычетом налогов ${yourWage(mony)}`);
+// } else {
+//     console.log('введите число');
+// }
+
+
+// function fun(num1, num2, num3) {
+//     return Math.max(num1, num2, num3)
+// }
+
+// let num1 = Number(prompt('Введите возраст'));
+// let num2 = Number(prompt('Введите возраст'));
+// let num3 = Number(prompt('Введите возраст'));
+
+// console.log(fun(num1, num2, num3));
+
+
+// let num = Number(prompt('Введите число'));
+// let num2 = Number(prompt('Введите число'));
+
+// function sum(num, num2) {
+//     return num + num;
+// }
+
+
+// const addition = (num1, num2) => num1 + num2;
+// const difference = (num1, num2) => {
+//     if (num1 === num2) return 0;
+//     return (num1 > num2) ? num1 - num2 : num2 - num1;
+// }
+// const multiplication = (num1, num2) => num1 * num2;
+// const division = (num1, num2) => num1 / num2;
+
+// console.log(addition(2, 7));
+// console.log(difference(2, 7));
+// console.log(difference(7, 2));
+// console.log(difference(7, 7));
+// console.log(multiplication(2, 7));
+// console.log(division(10, 5));
