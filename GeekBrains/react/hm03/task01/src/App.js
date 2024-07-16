d919eac49ec0a8499ace289e24b0bc80b7892a90
YuriyPidenko <120717893+YuriyPidenko@users.noreply.github.com>
2024-07-16 09:57:24 +0300

    dz03

---
+++ GeekBrains/react/hm03/task01/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TemperatureConverter from "./components/TemperatureConverter";
+
+const App = () => {
+    return ( <
+        div >
+        <
+        TemperatureConverter / > { " " } <
+        /div>
+    );
+};
+
+export default App;
