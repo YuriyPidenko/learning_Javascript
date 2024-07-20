5be0c9e12f7d6d5783dde83bc272e5c78eac0e44
YuriyPidenko <120717893+YuriyPidenko@users.noreply.github.com>
2024-07-20 19:34:40 +0300

    Add files via upload
    dz04

---
+++ GeekBrains/react/hm03/hm04/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Главная страница</Link>
+            </li>
+            <li>
+              <Link to="/about">О нас</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
